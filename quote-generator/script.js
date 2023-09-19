90432074f2d353772ed54e2955251fd30cb4cf99
jacodegithub <dudeobama246@gmail.com>
2023-09-19 17:03:37 +0530

    creating fetch service for fetching quotes

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,1 +1,16 @@
 // GET QUOTE FROM API
+async function getQuote() {
+
+    const apiUrl = 'https://api.quotable.io/random';
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch(error) {
+        console.log('whopps, no quote', error)
+    }
+}
+
+// ON LOAD
+getQuote();
